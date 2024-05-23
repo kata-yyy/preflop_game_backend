e5f2168b7f9357f219df8c24cee5411d155031ec
kata-yyy <larton-sub@yahoo.co.jp>
2024-05-24 08:33:27 +0900

    corsに本番のURLを追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3000" 
+    origins "localhost:3000", "preflop-game-frontend-3caa6ba8dc52.herokuapp.com"
 
     resource "*",
       headers: :any,
